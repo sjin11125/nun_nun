d6ecf394d90b48333b6f52370e72665c1868aae1
hanyeonju <hanyoenju@gmail.com>
2022-01-07 19:02:33 +0900

    220107
    keep아이템 오류

--- Assets/Scripts/Game/Grid/GridSquare.cs
+++ Assets/Scripts/Game/Grid/GridSquare.cs
@@ -17,15 +17,12 @@ public class GridSquare : MonoBehaviour
     public int SquareIndex { get; set; }
     public bool SquareOccupied { get; set; }
 
-    public GameObject keepTimer;
     public GameObject activeObj;
-
-    [HideInInspector]
-    public int keepCount, trashCount;
 
     public string currentColor;
     public string currentShape;
-    public GameObject KeepShape;
+    public Sprite keepImage;
+    public bool UseKeepBool = false;
 
     void Start()
     {
@@ -41,19 +38,6 @@ public class GridSquare : MonoBehaviour
             spriteImage = squareImage.GetComponent<Image>();
         }
     }
-
-    void Update()
-    {
-        if (keepTimer.activeSelf ==false)
-        {
-            keepCount = 0;
-        }
-        if (keepTimer.activeSelf == false)
-        {
-            trashCount = 0;
-        }
-    }
-
     //temp function remove it
     public bool CanWeUseThisSquare()
     {
@@ -72,10 +56,10 @@ public class GridSquare : MonoBehaviour
 
         Selected = true; //선택됨
         SquareOccupied = true; //사용중
-
+        
         if (activeImage.gameObject.activeSelf == true)
         {
-            activeImage.GetComponent<Image>().sprite = spriteImage.sprite;//쉐이프 스프라이트 전달
+            activeImage.GetComponent<Image>().sprite = spriteImage.sprite;//쉐이프 스프라이트 전달       
         }
     }
 
@@ -105,15 +89,16 @@ public class GridSquare : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)//충돌처음
     {
-        if(SquareOccupied == false)//사용중이 아니면
+        if (SquareOccupied == false)//사용중이 아니면
         {
             Selected = true;//선택된걸로바꿔
             hooverImage.gameObject.SetActive(true);//진한색
+            
             GameObject ShapeStorageObj = GameObject.FindGameObjectWithTag("ShapeStorage");
-            if (ShapeStorageObj != null)
-            {
+            if (ShapeStorageObj != null)//여기서 항상 들어갈때 shape의 정보를 받는다
+            {                             
                 currentColor = ShapeStorageObj.GetComponent<ShapeStorage>().shapeColor;
-                currentShape = ShapeStorageObj.GetComponent<ShapeStorage>().shapeShape;
+                currentShape = ShapeStorageObj.GetComponent<ShapeStorage>().shapeShape;               
             }
         }
         else if(collision.GetComponent<ShapeSquare>() != null)//쉐이프와 닿아있음
@@ -133,7 +118,7 @@ public class GridSquare : MonoBehaviour
         else if (collision.GetComponent<ShapeSquare>() != null)
         {
             collision.GetComponent<ShapeSquare>().SetOccupied();
-        }
+        }       
     }
 
     private void OnTriggerExit2D(Collider2D collision)//충돌벗어남
@@ -149,78 +134,32 @@ public class GridSquare : MonoBehaviour
         {
             collision.GetComponent<ShapeSquare>().UnSetOccupied();//레드라이트꺼
         }
-    }
 
-    GameObject twentyNine;
-    int keepSquareIndex;
-    //public GameObject prefab;
-    public void ColorTransfer() //그리드스크립트 UseKeep과 연결
-    {
-        GameObject ItemControllerObj = GameObject.FindGameObjectWithTag("ItemController");//컨트롤러에서 선택한 인덱스에 따라 위치 결정
-        if (ItemControllerObj != null)
+        GameObject GridObj = GameObject.FindGameObjectWithTag("Grid");
+        if (GridObj != null && UseKeepBool == true)
         {
-            keepSquareIndex = ItemControllerObj.GetComponent<ItemController>().keepItemIndex;
+            currentColor = GridObj.GetComponent<GridScript>().KeepColor;//나갈때확인해서 keep을 사용했던거라면 
+            currentShape = GridObj.GetComponent<GridScript>().KeepShape;
+            GridObj.GetComponent<GridScript>().CheckIfKeepLineIsCompleted();
+            GridObj.GetComponent<GridScript>().CheckIfKeepLineIsCompletedX();
+            //GridObj.GetComponent<GridScript>().CheckIfAnyLineIsCompleted();//함수실행순서로인해 바로 없어지지않고 다음턴에 없어지기 때문에 바로 실행
         }
-
-        GameObject contectGrid = GameObject.FindGameObjectWithTag("Grid");
-        if (contectGrid != null)
-        {
-            twentyNine = contectGrid.transform.GetChild(keepSquareIndex).gameObject; //29번오브젝트저장
-        }
-
-        if (Input.GetMouseButtonDown(0)) //좌클할때
-        {
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);//광선을 쏴
-            if (hit.collider != null)
-            {
-                if (hit.collider.gameObject == twentyNine && keepCount == 0)// && activeObj.activeSelf==true)//내가 누른게 29번오브젝트가 맞고 한번 눌렀고 액티브오브젝트가 켜져야
-                {
-                    keepOnclick();
-                    /*
-                    spriteImage.sprite = activeImage.GetComponent<Image>().sprite;//저장해둔 색깔을 shape에 줘
-                    GameObject useKeepShape = GameObject.FindGameObjectWithTag("ShapeStorage");//keep에있는 색깔과모양정보를 현재 shape에 준다
-                    if (useKeepShape != null)
-                    {
-                        useKeepShape.GetComponent<ShapeStorage>().shapeColor = currentColor;
-                        useKeepShape.GetComponent<ShapeStorage>().shapeShape = currentShape;
-                    }*/
-                    currentColor = null;//주고나서 비운다
-                    currentShape = null;
-                    keepTimer.SetActive(true);                  
-                    activeObj.SetActive(false);
-                    keepCount++;
-                }
-            }
-        }
+       
+        UseKeepBool = false;
     }
 
     public void TrashCan()
     {
         activeImage.sprite = normalImage.sprite;
-
-        if (trashCount == 0)
-        {  
-             keepTimer.SetActive(true);
-             activeObj.SetActive(false);
-             trashCount++;
-        }
     }
-    
-    public void keepOnclick()
+    public void UseSquareKeep()//킵 프리팹과 닿으면 켜지는 함수
     {
-        GameObject keepInstance = Instantiate(KeepShape) as GameObject;
-        keepInstance.transform.SetParent(this.transform, false);
-        Vector3 pos = new Vector3(0, 0, 0);
-        keepInstance.transform.localPosition = pos;
+        UseKeepBool = true;
+        hooverImage.gameObject.SetActive(false);//선택되고있는중에뜨는 진한색끄고
+        activeImage.gameObject.SetActive(true);//선택된 색 켜기
 
-        GameObject useKeepShape = GameObject.FindGameObjectWithTag("KeepShape");//keep에있는 색깔과모양정보를 현재 shape에 준다
-        if (useKeepShape != null)
-        {
-            useKeepShape.GetComponent<CreateKeepShape>().keepColor = currentColor;
-            useKeepShape.GetComponent<CreateKeepShape>().keepShape = currentShape;
-            useKeepShape.GetComponent<Image>().sprite = activeImage.sprite;
-        }
-        
+        Selected = true; //선택됨
+        SquareOccupied = true; //사용중
+        gameObject.transform.GetChild(2).gameObject.GetComponent<Image>().sprite = keepImage;
     }
 }
