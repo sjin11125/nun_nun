3e853be16d9af9ec8a5893c10170360436c4da9e
sjin11125 <69034338+sjin11125@users.noreply.github.com>
2022-02-11 11:41:04 +0900

    ui 애니메이션

---
+++ Assets/UIAniManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+public class UIAniManager : MonoBehaviour
+{
+
+    public RectTransform mainMenu, ShopMenu;
+
+    public RectTransform startMenu;
+
+    //public StartManager startmanager;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        mainMenu.DOAnchorPos(Vector2.zero, 0.5f); // 메인패널 처음에 중간세팅
+                                                  //startMenu.transform.localScale = Vector2.zero;
+    }
+
+    // Update is called once per frame
+
+    public void ShopButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-1000, 0), 0.5f);
+        ShopMenu.DOAnchorPos(new Vector2(0, 0), 0.5f);
+    }
+
+    public void CloseShopButton()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.5f);
+        ShopMenu.DOAnchorPos(new Vector2(0, 2000), 0.5f);
+    }
+
+    public void StartOpen()
+    {
+        //startMenu.transform.DOScale(Vector3.one, 0.3f);
+        //startmanager.CharacterOpen();
+
+    }
+
+    public void StartClose()
+    {
+        startMenu.transform.DOScale(Vector3.zero, 0.3f);
+
+    }
+
+    void Update()
+    {
+
+    }
+}
