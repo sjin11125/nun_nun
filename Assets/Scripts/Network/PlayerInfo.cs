99f5840e655a86c1a1f2517d85ece6fd46b29e71
sjin11125 <69034338+sjin11125@users.noreply.github.com>
2022-02-11 11:02:12 +0900

    friend

--- Assets/Scripts/Network/PlayerInfo.cs
+++ Assets/Scripts/Network/PlayerInfo.cs
@@ -8,6 +8,7 @@ public class PlayerInfo : MonoBehaviour
     public static string Id;            //플레이어 아이디
     public static string NickName;      //플레이어 닉네임
     public static string SheetsNum;     //플레이어 건물 정보 들어있는 스프레드 시트 id
+    public static string Info;          //상태메세지
 
     string[] Friends;       //친구 목록(닉네임)
     // Start is called before the first frame update
