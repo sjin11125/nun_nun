d18d0c3a109ebc5b15d974ab6223da1dd12d6e34
Vicky <69034338+sjin11125@users.noreply.github.com>
2022-04-25 21:08:58 +0900

    Update CanvasManger.cs

--- Assets/Scripts/ScriptM/CanvasManger.cs
+++ Assets/Scripts/ScriptM/CanvasManger.cs
@@ -7,21 +7,18 @@ public class CanvasManger : MonoBehaviour
 {
     //canvas에 텍스트랑 재화 연결해라
     public Text Money;          //재화
-    public Text Tree;          //재화
-    public Text Snow;          //재화
-    public Text Ice;          //재화
-    public Text Grass;          //재화
-    public Text Gem;        //잼
+    public Text ShinMoney;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Money.text = GameManager.Money.ToString();
-        Gem.text = GameManager.Gem.ToString();
+        ShinMoney.text = GameManager.ShinMoney.ToString();
     }
+    
 }
