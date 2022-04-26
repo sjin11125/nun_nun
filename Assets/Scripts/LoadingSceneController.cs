0a4ed7377c46691396d56d0e57e99e3f65de64f4
hanyeonju <hanyoenju@gmail.com>
2022-04-26 18:37:55 +0900

    로딩

--- Assets/Scripts/LoadingSceneController.cs
+++ Assets/Scripts/LoadingSceneController.cs
@@ -113,17 +113,4 @@ public class LoadingSceneController : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
-
-// Start is called before the first frame update
-void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-
-    void Update()
-    {
-        
-    }
 }
