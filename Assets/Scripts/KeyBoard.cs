c99c67a3728d24852543a00c25456f0189bf62bc
hanyeonju <hanyoenju@gmail.com>
2022-04-27 01:25:20 +0900

    2

--- Assets/Scripts/KeyBoard.cs
+++ Assets/Scripts/KeyBoard.cs
@@ -8,17 +8,12 @@ public class KeyBoard : MonoBehaviour
     TouchScreenKeyboard id;
     string saveID;//여기에 저장
 
-    TouchScreenKeyboard password;
-    string savePassword;//여기에 저장
 
     public void OpenIDKeyBoard()
     {
        id= TouchScreenKeyboard.Open("",TouchScreenKeyboardType.Default);
     }
-    public void OpenPasswordKeyBoard()
-    {
-        password = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
-    }
+
 
     public void Update()
     {
@@ -28,15 +23,6 @@ public class KeyBoard : MonoBehaviour
             {
                 saveID = id.text;
                 id = null;
-            }
-        }
-
-        if (TouchScreenKeyboard.visible == false && password != null)
-        {
-            if (password.done)//엔터 눌렀어
-            {
-                savePassword = password.text;
-                password = null;
             }
         }
     }
