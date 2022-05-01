da4f2e2ff4a8f51d88b15c13f1f8eafb1fca7165
Vicky <69034338+sjin11125@users.noreply.github.com>
2022-05-02 00:59:42 +0900

    친구수정

--- Assets/Scripts/Scripts_Store/GameManager.cs
+++ Assets/Scripts/Scripts_Store/GameManager.cs
@@ -66,7 +66,7 @@ public class GameManager : MonoBehaviour
 
     public static FriendInfo[] Friends;       //친구 목록(닉네임)
 
-    public static string URL = "https://script.google.com/macros/s/AKfycbyzhYNNB7KxK6DnqU8Ok_J9p57NridPIu__6fkzxwH6x1fByFJ5FnzqvKhFGxJBg7Xw/exec";
+    public static string URL = "https://script.google.com/macros/s/AKfycbzIIcubQsqzF2j8W8fs1eH5PVDdNkEfvD2-NY4nX8YAc6QlVFGNceGpr1b-ueELNLpj/exec";
     //----------------------------------------------------------------------------------------------
 
 
