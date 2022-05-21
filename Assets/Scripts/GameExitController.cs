41e1e43611bc3da3588adef6dd0f3930ce9b8fe9
hanyeonju <hanyoenju@gmail.com>
2022-05-21 17:44:54 +0900

    재화랑 수입창 제어

--- Assets/Scripts/GameExitController.cs
+++ Assets/Scripts/GameExitController.cs
@@ -32,35 +32,7 @@ public class GameExitController : MonoBehaviour
         //isMe = true;                 
         form2.AddField("order", "setMoney");
         form2.AddField("player_nickname", GameManager.NickName);
-        form2.AddField("money", GameManager.Money.ToString()+"@"+GameManager.ShinMoney.ToString());
-
-        StartCoroutine(MoneyPost(form2));
-    }
-    IEnumerator MoneyPost(WWWForm form)
-    {
-        using (UnityWebRequest www = UnityWebRequest.Post(GameManager.URL, form)) 
-        {
-            yield return www.SendWebRequest();
-            //Debug.Log(www.downloadHandler.text);
-            if (www.isDone)
-            {
-                MoneyResponse(www.downloadHandler.text);
-            }
-        }
-
-    }
-
-    void MoneyResponse(string json)                        
-    {
-        if (string.IsNullOrEmpty(json))
-        {
-            Debug.Log(json);
-            return;
-        }
-        string[] moneys = json.Split('|');
-
-        GameManager.Money = int.Parse(moneys[0]);
-        GameManager.ShinMoney = int.Parse(moneys[1]);
+        //form2.AddField("money", GameManager.Money.ToString()+"@"+GameManager.ShinMoney.ToString());
 
     }
 
