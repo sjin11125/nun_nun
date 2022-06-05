a70713b5d618f492fc8700890a29a8d89064aaf2
sjin11125 <69034338+sjin11125@users.noreply.github.com>
2022-06-05 22:04:35 +0900

    Update GameManager.cs

--- Assets/Scripts/Scripts_Store/GameManager.cs
+++ Assets/Scripts/Scripts_Store/GameManager.cs
@@ -85,7 +85,7 @@ public class GameManager : MonoBehaviour
 
     public static string friend_nickname;       //현재 들어가있는 친구닉넴
 
-    public static string URL = "https://script.google.com/macros/s/AKfycbyOxgEK8U_LnKRHOUxKHt6pxmjdlGUz7ES1kE5048qoDRUeEupawtAUnhAWAgzqAE90/exec";
+    public static string URL = "https://script.google.com/macros/s/AKfycby-EjR17z85nt-BGDuj6kd7vi30djdGX6rrRjLRWMfWB1f1ghMo6roLWGRc_BFy8b2k/exec";
 
     public static bool isReward;        //일괄수확 가능한지
     
