959f7e619083ba2e385006e52d77677f23b3de17
sjin11125 <69034338+sjin11125@users.noreply.github.com>
2022-06-13 11:55:27 +0900

    친구 불러오기 수정

--- Assets/Scripts/Scripts_Store/GameManager.cs
+++ Assets/Scripts/Scripts_Store/GameManager.cs
@@ -85,7 +85,7 @@ public class GameManager : MonoBehaviour
 
     public static string friend_nickname;       //현재 들어가있는 친구닉넴
 
-    public static string URL = "https://script.google.com/macros/s/AKfycbwvvNkXtpws9XzlvbWkHnMzMg8i0EVSBEb5qyinCMT4Jksh5U7Vv3Xk4JPJRqPgQ8L5/exec";
+    public static string URL = "https://script.google.com/macros/s/AKfycbyF05TU_zroiT1GfoQclxmmJTX4K2jZj1UqV8ZJSRbcHy-2Y3ApEqhajz9E29qe8RPL/exec";
 
     public static bool isReward;        //일괄수확 가능한지
     
