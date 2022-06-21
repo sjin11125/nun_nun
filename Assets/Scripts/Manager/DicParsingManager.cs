a9a5442c0ca20cca459fd5802e4f41d548447444
Vicky <69034338+sjin11125@users.noreply.github.com>
2022-06-22 02:31:27 +0900

    스토어 업데이트

---
+++ Assets/Scripts/Manager/DicParsingManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DicParsingManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    public static Character[] Mal;
+    TextAsset csvData;
+    void Start()
+    {
+        if (GameManager.parse.Equals(false))
+        {
+            GameManager.parse = true;
+        }
+    }
+    public Card[] Parse_character(int index)                //누니 정보 불러옴 
+    {
+        List<Card> CharacterList = new List<Card>();
+        if (index .Equals( 1))
+        {
+            csvData = Resources.Load<TextAsset>("Character");    //csv파일 가져옴
+
+        }
+        string[] data = csvData.text.Split(new char[] { '\n' });    //엔터 기준으로 쪼갬.
+
+
+        for (int i = 1; i < data.Length - 1; i++)
+        {
+            string[] pro_data = data[i].Split(',');
+
+            if (pro_data[0] .Equals( "end"))
+            {
+                break;
+            }
+            if (pro_data[10].Contains("@"))
+            {
+                pro_data[10]= pro_data[10].Replace("@",",");
+            }
+            Card character = new Card(pro_data[1], pro_data[2], pro_data[3], pro_data[4], pro_data[5], pro_data[6],
+                                       pro_data[7], pro_data[8], pro_data[9], pro_data[10], pro_data[11], pro_data[12], pro_data[13]);
+            //잠금    /   이름  /  아이템 /   이미지 /  가격  /  레벨  /  별   /  게이지 /  설명  / 보유효과  / 건물  / 골드 획득량
+            character.SetChaImage(GameManager.CharacterImageData[pro_data[4]]);
+            CharacterList.Add(character);
+        }
+
+        for (int i = 0; i < CharacterList.Count; i++)
+        {
+            string ImageName = CharacterList[i].cardImage;
+            if (GameManager.CharacterImageData.ContainsKey(ImageName))
+            {
+                CharacterList[i].SetChaImage(GameManager.CharacterImageData[ImageName]);     //누니 이미지 넣기     
+
+            }
+        }
+        return CharacterList.ToArray();
+
+    }
+
+    //-----------------------건물 정보---------------------------------
+
+
+    public Building[] Parse(int index)
+    {
+        if (GameManager.parse .Equals( false))
+        {
+            GameManager.parse = true;
+        }
+        
+        List<Building> DictionaryList = new List<Building>(); //도감 캐릭터 리스트 생성.
+        List<Building> DictionaryStrList = new List<Building>(); //도감 캐릭터 리스트 생성.
+        if (index.Equals(0))
+        {
+            csvData = Resources.Load<TextAsset>("Dogam");    //csv파일 가져옴
+        }
+
+        string[] data = csvData.text.Split(new char[] { '\n' });    //엔터 기준으로 쪼갬.
+
+      
+        
+        for (int i = 1; i < data.Length-1; i++)
+        {
+            string[] pro_data = data[i].Split(',');
+
+            if (pro_data[0].Equals("end"))
+            {
+                break;
+            }
+           // Character character = new Character(pro_data[1],pro_data[2], pro_data[3], pro_data[4], pro_data[5], pro_data[6], pro_data[7], pro_data[8], pro_data[9]);
+            Building building = new Building(pro_data[1], pro_data[2], pro_data[3], pro_data[4], pro_data[5], pro_data[6], pro_data[7], pro_data[8], pro_data[9], pro_data[10], pro_data[12]);
+            building.Level = 1;
+            //잠금 유무     // 이름     //설명     //이미지    //가격1       //가격2      //가격3        //생성재화1         //생성재화2        //생성재화3      //설치물인지
+            if (pro_data[11] .Equals( "T") )         //설치물인가
+            {
+                DictionaryStrList.Add(building);
+            }
+            else                                //설치물 아닌가                               
+            {
+                DictionaryList.Add(building);
+            }
+
+        }
+        GameManager.StrArray = DictionaryStrList.ToArray();
+     
+        return DictionaryList.ToArray();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,268 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameManager : MonoBehaviour
+{   
+    public static bool isStart = false;
+    static GameManager _Instance;
+    public static bool parse = false;
+    public Sprite[] DogamChaImageInspector;     //인스펙터에서 받아 온 건물 이미지
+
+    public static Sprite[] DogamChaImage;
+    public static Dictionary<string, Sprite> DogamChaImageData;
+
+    public static List<Building> BuildingList;          //가지고 있는 빌딩들
+    public static List<Building> StrList;          //가지고 있는 설치물들
+    public static List<Building> FriendBuildingList;          //친구가 가지고 있는 빌딩들
+    public static Building[] BuildingArray;         //모든 빌딩들
+    public static Building[] StrArray;         //모든 빌딩들
+
+    public GameObject[] BuildingPrefabInspector;    //인스펙터에서 받아 온 건물 프리팹 배열    
+
+    public static Dictionary<string, GameObject> BuildingPrefabData;    //모든 빌딩 프리팹 딕셔너리
+
+    public static GameObject CurrentBuilding;       //현재 수정중인 건물
+
+    public static Building CurrentBuilding_Script;
+
+
+    public static InventoryButton CurrentBuilding_Button=null;            //현재 수정중인 인벤 버튼
+
+
+    public static Dictionary<string, int> BuildingNumber;            //건물이 종류별로 몇개 있는지(건물번호)
+
+    public static bool isEdit = false;
+    public static bool isInvenEdit = false;
+    public static Button InvenButton;
+
+    public static List<string> IDs;        //건물 아이디
+    public static bool isUpdate = false;        //건물 목록 강제로 업데이트
+
+    public static bool isLoading = false;           //건물 다 로딩됐는지
+    //----------------------------------------------------이까지 건물----------------------------------------------------
+
+
+    //----------------------------------------------------여기서부터 누니--------------------------------------------------
+    public Sprite[] CharacterImageInspector;            // 인스펙터에서 받아 온 모든 누니들의 이미지
+    public static Dictionary<string, Sprite> CharacterImageData;
+
+    public GameObject[] CharaterPrefabInspector;        // 인스펙터에서 받아 온 모든 누니들의 프리팹
+    public static Dictionary<string, GameObject> CharacterPrefab;       //모든 캐릭터 누니 딕셔너리
+
+    public static Card[] AllNuniArray;              //엑셀에서 받아 온 모든 누니 정보 배열
+
+    public static List<Card> CharacterList;      //현재가지고 있는 누니 리스트
+    //public static Card[] CharacterArray;               //현재 가지고 있는 캐릭터 배열
+    
+
+    public static bool[] Items=new bool[10];     //현재 가지고 잇는 아이템 유무
+    public static int items=0;
+    public static bool isStore = false;
+
+    public GameObject Dont;
+    public static bool nuniDialogParse = false;
+
+    public static List<NuniDialog> NuniDialog;          //누니 상호작용 대화 
+    public Card CurrentNuni;
+    //-----------------------------------여기서부터 재화---------------------------------
+    public static int Money;            //재화
+    public static int ShinMoney;
+
+    //---------------------------------------------------------------------------------------------
+    //--------------------------------여기서부터 플레이어 정보-------------------------------------
+
+    public static string Id;            //플레이어 아이디
+    public static string NickName;      //플레이어 닉네임
+    public static string StateMessage;      //플레이어 상태메세지
+    public static string SheetsNum;     //플레이어 건물 정보 들어있는 스프레드 시트 id
+    public static Sprite ProfileImage;       //플레이어 프로필 이미지
+
+    public static FriendInfo[] Friends;       //친구 목록(닉네임)
+
+    public static string friend_nickname;       //현재 들어가있는 친구닉넴
+
+    public static string URL = "https://script.google.com/macros/s/AKfycbygPIjJEOtQLzvs_Xq1UFsuSA2W8veTgQ9Fjv08Ox-iX8wn49I_Ggo_smnXScoa5ImP/exec";
+
+    public static bool isReward;        //일괄수확 가능한지
+    
+    //----------------------------------------------------------------------------------------------
+
+
+    public static bool isMoveLock = false;      //창 떴을 때 이동 못하게하는 변수
+    /* 아이템 목록
+     * 0: 지우개               (황제)
+     * 1: 킵                   (비서)
+     * 2: 쓰레기통             (청소부)
+     * 3: 미리보기             (탐정)
+     * 4: 새로고침             (개발자)
+     * 5: <=>                  (과학자)
+     * 6: 가로3개              (팡팡)
+     * 7: 세로3개              (펑펑)
+     * 8: 모든 대체할수 있는 말(유니콘)
+     * 9: 말의 색깔을 바꾼다   (마법사)
+     */
+    // Start is called before the first frame update
+
+    //--------------------------------------------------------------------퀘스트---------------------------------------------------
+
+   public static QuestInfo[] Quest;                 //퀘스트 목록
+    public static QuestInfo[] QuestProgress;        //퀘스트 진행상황
+    public static bool isReset;             //퀘스트 초기화 햇니?
+    public static bool QParse = false;
+    public static bool[] QuestActive;                 //블록 얼마 깻는지 확인
+    public static int QuestColor = 0;                   //뭔 블록 깨야하는지    
+
+    public static bool isStrEdit = false;
+
+    public static bool gameMusicOn = true;
+    public static bool mainMusicOn = true;
+    public static bool gameSoundOn = true;
+    public static bool mainSoundOn = true;
+    private void Awake()
+    {
+        //PlayerPrefs.DeleteAll();
+        if (_Instance == null)
+        {
+            _Instance = this;
+            isStart = true;
+        }
+        else if (_Instance != this) // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);  // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
+    }
+
+    void Start()
+    {
+        BuildingList = new List<Building>();            //현재 가지고 있는 빌딩 리스트
+        //
+        DogamChaImageData = new Dictionary<string, Sprite>();       //전체 캐릭터 리스트(가지고 있지 않은것도 포함)
+        BuildingPrefabData = new Dictionary<string, GameObject>();      //전체 빌딩 프리팹 리스트 (가지고 있지 않은 것도 포함)
+
+        CharacterPrefab = new Dictionary<string, GameObject>();
+        CharacterImageData = new Dictionary<string, Sprite>();
+        CharacterList = new List<Card>();
+        BuildingNumber = new Dictionary<string, int>();
+        IDs = new List<string>();
+        Quest = new QuestInfo[3];                     //퀘스트 
+        NuniDialog = new List<NuniDialog>();
+      
+        for (int i = 0; i < BuildingPrefabInspector.Length; i++)        //빌딩 프리팹 정보 불러오기
+        {
+            BuildingPrefabData.Add(BuildingPrefabInspector[i].name+ "(Clone)", BuildingPrefabInspector[i]);
+  
+           
+        }      
+      
+        //일단 시작하면 전체 빌딩 프리팹 리스트에서 이름 받아서 임시로 0으로 초기화
+        for (int i = 0; i < BuildingPrefabInspector.Length; i++)
+        {
+            BuildingNumber.Add(BuildingPrefabInspector[i].name + "(Clone)",0);
+        }
+
+        for (int i = 0; i < DogamChaImageInspector.Length; i++)     // 빌딩 이미지 불러오기
+        {
+            DogamChaImageData.Add(DogamChaImageInspector[i].name, DogamChaImageInspector[i]);
+        }
+        for (int i = 0; i < CharacterImageInspector.Length; i++)        //캐릭터 
+        {
+            CharacterImageData.Add(CharacterImageInspector[i].name, CharacterImageInspector[i]);
+        }
+        for (int i = 0; i < CharaterPrefabInspector.Length; i++)
+        {
+            CharacterPrefab.Add(CharaterPrefabInspector[i].name, CharaterPrefabInspector[i]);
+        }
+
+        if (SceneManager.GetActiveScene().name.Equals("Main"))
+        {
+            //Dont.SetActive(true);
+        }
+
+        //게임 시작했을 때 엑셀에서 모든 누니 정보들 파싱해 배열에 넣기
+
+
+        DicParsingManager DPManager = new DicParsingManager();
+        AllNuniArray = DPManager.Parse_character(1);            //누니 정보 파싱
+        BuildingArray = DPManager.Parse(0);    //도감 정보 파싱
+
+       
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_Instance ==null)
+            {
+                return null;
+            }
+            return _Instance;
+        }
+    }
+    
+    public static Sprite GetDogamChaImage(string ImageName)
+    {
+        return DogamChaImageData[ImageName.Trim()];
+
+    }
+    public static Sprite GetCharacterImage(string ImageName)
+    {
+        
+        return CharacterImageData[ImageName.Trim()];
+    }
+
+    public static string IDGenerator()
+    {
+        string alpha= "qwertyuipoasdfjkl123456789";
+        string id="";
+
+        bool isCount = false;
+        do
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                id += alpha[Random.Range(0, 24)];
+            }
+            for (int i = 0; i < IDs.Count; i++)
+            {
+                if (IDs[i] .Equals( id))
+                {
+                    isCount = false;
+                }
+                else
+                {
+                    isCount = true;
+                    IDs.Add(id);
+                }
+            }
+        } while (isCount .Equals( true));
+        return id;
+    }
+
+    public void GameSave()
+    {
+        PlayerPrefs.SetInt("Money", Money);
+        PlayerPrefs.SetInt("ShinMoney", ShinMoney);
+        PlayerPrefs.Save();
+        print("save");
+
+        WWWForm form2 = new WWWForm();
+        //isMe = true;                 
+        form2.AddField("order", "setMoney");
+        form2.AddField("player_nickname", NickName);   
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (pause)//비활성화
+        {
+            GameSave();
+        }
+    }
+}
