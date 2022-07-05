3d31127db31aa183a130a291db23f6606209f11b
hanyeonju <hanyoenju@gmail.com>
2022-07-06 03:26:57 +0900

    업적하는중

---
+++ Assets/Scripts/Game/AchieveContent.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchieveContent : MonoBehaviour
+{
+    public Button getBtn;
+    public Text con_text;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
