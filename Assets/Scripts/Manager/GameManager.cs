3059a0a32419b5e563811f1efd4db7fb15913f31
sjin11125 <69034338+sjin11125@users.noreply.github.com>
2022-07-07 17:20:14 +0900

    Revert "Merge branch 'newDBTest' into mibal"
    This reverts commit 86fd93a251e9c7ea4cf946b5e3b98cf4c926ef9f, reversing
    changes made to 4d85bf76f86bfa948725fa9b4a3097b1b60500a8.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -76,7 +76,6 @@ public class GameManager : MonoBehaviour
     //-----------------------------------여기서부터 재화---------------------------------
     public static int Money;            //재화
     public static int ShinMoney;
-    public static int Zem;
 
     
     //---------------------------------------------------------------------------------------------
