0253f697aae9ac0583c3c8686de35fb422ba1515
Vicky <69034338+sjin11125@users.noreply.github.com>
2023-01-08 14:01:45 +0900

    .

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const { initializeApp } = require('firebase-admin/app');
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
